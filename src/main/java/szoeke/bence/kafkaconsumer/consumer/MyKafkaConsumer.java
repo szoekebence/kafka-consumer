a4c3e1b75ce08b13b6acae9b9d4dfaf0d052ca9d
Szőke Bence <bence.szoke@idomsoft.hu>
2022-09-10 21:28:05 +0200

    fix logging

--- src/main/java/szoeke/bence/kafkaconsumer/consumer/MyKafkaConsumer.java
+++ src/main/java/szoeke/bence/kafkaconsumer/consumer/MyKafkaConsumer.java
@@ -1,10 +1,10 @@
 package szoeke.bence.kafkaconsumer.consumer;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.VoidDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,42 +19,28 @@ public class MyKafkaConsumer {
     private static final String TOPIC_NAME_ENV_VAR = "TOPIC_NAME";
     private static final String TOPIC_NAME = System.getenv(TOPIC_NAME_ENV_VAR);
     private final Properties properties;
-    private long sequenceNumber = 0;
-    private long maxKeyValueReceived = 0;
+    private long numOfEvents = 0;
 
     public MyKafkaConsumer() {
         properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, System.getenv(BOOTSTRAP_SERVER_ENV_VAR));
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "MyConsumerGroup");
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "szokeb-consumer-group");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, VoidDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
     }
 
     public void consumeRecords() {
-        try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties)) {
+        try (KafkaConsumer<Void, String> consumer = new KafkaConsumer<>(properties)) {
             consumer.subscribe(Collections.singleton(TOPIC_NAME));
             doConsuming(consumer);
         }
     }
 
-    private void doConsuming(KafkaConsumer<String, String> consumer) {
+    private void doConsuming(KafkaConsumer<Void, String> consumer) {
         while (true) {
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
-            for (ConsumerRecord<String, String> record : records) {
-                doLogging(record);
-            }
-        }
-    }
-
-    private void doLogging(ConsumerRecord<String, String> record) {
-        long key = Long.parseLong(record.key());
-        if (record.value().length() < 10) {
-            sequenceNumber += Long.parseLong(record.value());
-            LOGGER.info("Record read successfully with key | value: {} | {} \n" +
-                    "Record count: {}", key, record.value(), sequenceNumber);
-        } else if (key > maxKeyValueReceived) {
-            maxKeyValueReceived = key;
-            LOGGER.info("Record read successfully with key: {}", maxKeyValueReceived);
+            ConsumerRecords<Void, String> records = consumer.poll(Duration.ofMillis(1000));
+            numOfEvents += records.count();
+            LOGGER.info("Number of read records: {}.", numOfEvents);
         }
     }
 }
